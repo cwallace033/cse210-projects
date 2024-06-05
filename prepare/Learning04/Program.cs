aa81e82cb82ace89602b319da0c7b07e193b9325
cwallace033 <cmwallce033@gmail.com>
2024-06-05 13:03:09 -0600

    completed learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Chase Wallace", "Coding");
+
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Chase Wallace", "Fractions", "7.3", "8-19");
+
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Chase Wallace", "Coding", "Coding for Dummies");
+
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
+
     }
 }
